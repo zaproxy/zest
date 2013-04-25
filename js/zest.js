04024b25d278a346bd30d7d0619b30285de2be6e
Mark Goodwin <mgoodwin@mozilla.com>
2013-04-25 17:44:21 +0100

    some tidy up, a start on testing

--- js/zest.js
+++ js/zest.js
@@ -1,10 +1,11 @@
-zest = require('./zestscript');
-fs = require('fs');
+var zest = require('./zestscript');
+var fs = require('fs');
+var operations = require('./node-operations');
 
 process.argv.splice(2).forEach(function (val, index, array) {
   fs.readFile(val, 'utf8', function(err, data) {
     if (err) throw err;
-    var script = zest.ZestScript.parse(data);
+    var script = zest.ZestScript.parse(data, operations);
     script.run();
   });
 });
