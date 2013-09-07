80a2ea842d1760fc2c9c01d7e77ab7eadb276961
psiinon <psiinon@gmail.com>
2013-09-07 16:36:39 +0100

    Added public method: void add(int index, ZestStatement stmt)

--- src/org/mozilla/zest/core/v1/ZestLoop.java
+++ src/org/mozilla/zest/core/v1/ZestLoop.java
@@ -121,6 +121,28 @@ public abstract class ZestLoop<T> extends ZestStatement implements
 	}
 
 	/**
+	 * Adds the statement in the specified index in the script.
+	 *
+	 *@param index the index at which the statement will be added
+	 * @param stmt the statement to add
+	 */
+	public void add(int index, ZestStatement stmt) {
+		ZestStatement prev = this;
+		if (index == this.statements.size()) {
+			// Add at the end
+			this.statements.add(stmt);
+			
+		} else {
+			this.statements.add(index, stmt);
+		}
+		if (index > 0) {
+			prev = this.statements.get(index-1);
+		}
+		// This will wire everything up
+		stmt.insertAfter(prev);
+	}
+
+	/**
 	 * returns the current state of the loop.
 	 * 
 	 * @return the current state
