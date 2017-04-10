d4f7d27fad26faa2663edc99222ffeef32297cd6
thc202 <thc202@gmail.com>
2017-04-10 14:08:26 +0100

    Add LICENSE and normalise license header
    Add a top level LICENSE file with MPL2.
    Add Spotless Gradle plugin and the license header for Java source files,
    to ensure all Java source files have the expected license header (the
    files were updated accordingly).
    Update Travis CI build configuration file to use JDK 8 (required by
    Spotless plugin).

--- src/test/java/org/mozilla/zest/test/v1/ZestConditionalRegexExprUnitTest.java
+++ src/test/java/org/mozilla/zest/test/v1/ZestConditionalRegexExprUnitTest.java
@@ -1,11 +1,6 @@
-/**
 /* This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
- * file, You can obtain one at http://mozilla.org/MPL/2.0/.
- * 
- * @author Alessandro Secco: seccoale@gmail.com
- */
-
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 package org.mozilla.zest.test.v1;
 
 import static org.junit.Assert.assertEquals;
