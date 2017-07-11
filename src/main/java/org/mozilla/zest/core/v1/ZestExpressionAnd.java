67fdb1819707cd02349a4d1439c29a10246c65ae
thc202 <thc202@gmail.com>
2017-07-11 22:33:05 +0100

    Remove misplaced license headers
    Remove license header from JavaDoc of the classes ZestExpressionElement
    and ZestExpressionAnd.

--- src/main/java/org/mozilla/zest/core/v1/ZestExpressionAnd.java
+++ src/main/java/org/mozilla/zest/core/v1/ZestExpressionAnd.java
@@ -8,11 +8,6 @@ import java.util.List;
 
 // TODO: Auto-generated Javadoc
 /**
- * 
- /* This Source Code Form is subject to the terms of the Mozilla Public
- * License, v. 2.0. If a copy of the MPL was not distributed with this file, You
- * can obtain one at http://mozilla.org/MPL/2.0/.
- * 
  * This class represent a List of AND clauses
  * 
  * @author Alessandro Secco: seccoale@gmail.com
